1f9c41d1d976c0ab9ef852615a38db4c84896908
drishelom <dofficaldiscord@hotmail.com>
2025-11-12 18:40:22 +0300

    added main functionality + fixed major performance issue

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include<SDL3_ttf/SDL_ttf.h>
 #include<muparser/muParser.h>
 #include<iostream>
+#include<codecvt>
 
 // NAMESPACES
 using namespace std;
@@ -16,11 +17,14 @@ using namespace std;
 //CONSTANTS
 int WINDOW_WIDTH=1280;
 int WINDOW_HEIGHT=720;
+const float MIN_ZOOM=.005, MAX_ZOOM=1, ZOOM_INCREMENT=.01;
 
 // VARIABLES
-SDL_Point GRAPH_POS={0, 0};
+SDL_Point GRAPH_POS={WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
+string EXPRESSION="";
 float GRAPH_SCALE=.05;
-mu::string_type EXPRESSION;
+float mPosX=0, mPosY=0;
+bool mouse1=false, mouse2=false;
 
 // SDL3
 static SDL_Window* win;
@@ -31,6 +35,29 @@ TTF_Font* TEXT_FONT;
 
 // MUPARSER
 mu::Parser parser;
+double GLOBAL_X=NAN;
+bool IS_EXPRESSION_VALID=false;
+
+// VALIDITY CHECK
+void validateExpr(){
+    try{
+        parser.Eval();
+        IS_EXPRESSION_VALID=true;
+    }catch(mu::Parser::exception_type &e){
+        IS_EXPRESSION_VALID=false;
+    }
+}
+
+// EVALUATING EXPRESSION
+double evalExpr(double x){
+    GLOBAL_X=x*GRAPH_SCALE;
+    if (isnan(GLOBAL_X) || !IS_EXPRESSION_VALID) return NAN;
+    try{
+        return -parser.Eval()/GRAPH_SCALE;
+    }catch(mu::Parser::exception_type &e){
+        return NAN;
+    }
+}
 
 // C-STRING CONVERSION
 const char* strToC(string a){
@@ -52,9 +79,12 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]){
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Init(SDL_INIT_EVENTS);
 	TTF_Init();
-	SDL_CreateWindowAndRenderer("Graph Renderer", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &win, &rendd);
+	SDL_CreateWindowAndRenderer("Graph Renderer", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL, &win, &rendd);
 	TEXT_FONT=TTF_OpenFont(MAIN_FONT, DEFAULT_FONT_SIZE);
+    parser.DefineVar("x", &GLOBAL_X);
 	parser.SetExpr(EXPRESSION);
+    validateExpr();
+    SDL_StartTextInput(win);
 	return SDL_APP_CONTINUE;
 }
 
@@ -65,7 +95,36 @@ SDL_AppResult SDL_AppIterate(void* appstate){
 	//cleanup
 	SDL_SetRenderDrawColor(rendd, BG_COLOR.r, BG_COLOR.g, BG_COLOR.b, BG_COLOR.a);
 	SDL_RenderClear(rendd);
-	//shit
+	//set blendmode
+    SDL_SetRenderDrawBlendMode(rendd, SDL_BLENDMODE_BLEND);
+    //render center point
+    SDL_SetRenderDrawColor(rendd, 155, 100, 255, 155);
+    SDL_RenderLine(rendd, GRAPH_POS.x, 0, GRAPH_POS.x, WINDOW_HEIGHT);
+    SDL_RenderLine(rendd, 0, GRAPH_POS.y, WINDOW_WIDTH, GRAPH_POS.y);
+    SDL_SetRenderDrawColor(rendd, 200, 200, 200, 255);
+    //render graph lines
+    float lastX=NAN, lastY=NAN;
+    for (float x=-GRAPH_POS.x; x<WINDOW_WIDTH-GRAPH_POS.x; x++){
+        float y=evalExpr(x);
+        float newX=x + GRAPH_POS.x;
+        float newY=y + GRAPH_POS.y;
+        if (!isnan(newY) && !isnan(lastY)){
+            SDL_RenderLine(rendd, lastX, lastY, newX, newY);
+        }
+        lastX=newX;
+        lastY=newY;
+    }
+    //render intersection
+    if (mouse2){
+        SDL_SetRenderDrawColor(rendd, 255, 155, 155, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(rendd, mPosX, 0, mPosX, WINDOW_HEIGHT);
+        float localY=evalExpr(mPosX - GRAPH_POS.x) + GRAPH_POS.y;
+        if (!isnan(localY)){
+            SDL_RenderLine(rendd, 0, localY, WINDOW_WIDTH, localY);
+        }
+    }
+    //render text
+    drawText(rendd, TEXT_FONT, TEXT_COLOR, BG_COLOR, {0, 0}, EXPRESSION);
 	//rendering
 	SDL_RenderPresent(rendd);
 	return SDL_APP_CONTINUE;
@@ -77,7 +136,42 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event){
 		case SDL_EVENT_QUIT:
 			return SDL_APP_SUCCESS;
 		case SDL_EVENT_KEY_DOWN:
+            if (event->key.scancode==SDL_SCANCODE_BACKSPACE){
+                if (!EXPRESSION.empty()) EXPRESSION.pop_back();
+                parser.SetExpr(EXPRESSION);
+                validateExpr();
+			}
 			break;
+		case SDL_EVENT_MOUSE_WHEEL:
+            GRAPH_SCALE=clamp<float>(GRAPH_SCALE - event->wheel.y*ZOOM_INCREMENT, MIN_ZOOM, MAX_ZOOM);
+            break;
+        case SDL_EVENT_MOUSE_BUTTON_DOWN:
+            if (event->button.button==1){
+                mouse1=event->button.down;
+            }else if(event->button.button==3){
+                mouse2=event->button.down;
+            }
+            break;
+        case SDL_EVENT_MOUSE_BUTTON_UP:
+            if (event->button.button==1){
+                mouse1=event->button.down;
+            }else if(event->button.button==3){
+                mouse2=event->button.down;
+            }
+            break;
+        case SDL_EVENT_MOUSE_MOTION:
+            if (mouse1 && event->motion.state==1){
+                GRAPH_POS.x+=event->motion.xrel;
+                GRAPH_POS.y+=event->motion.yrel;
+            }
+            mPosX=event->motion.x;
+            mPosY=event->motion.y;
+            break;
+    	case SDL_EVENT_TEXT_INPUT:
+            EXPRESSION.append(event->text.text);
+            parser.SetExpr(EXPRESSION);
+            validateExpr();
+            break;
 	}
 	return SDL_APP_CONTINUE;
 }
